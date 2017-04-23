b72f24beaf98c6f58750e6a0a1bf51c23137b9e1
hakanyalp <hakanyavuzalp@hotmail.com>
2017-04-23 19:34:17 +0300

    23.04.2017 güncelleme

--- CpuSimülasyon/Form1.Designer.cs
+++ CpuSimülasyon/Form1.Designer.cs
@@ -34,6 +34,24 @@
             this.lblPC = new System.Windows.Forms.Label();
             this.lblMBR = new System.Windows.Forms.Label();
             this.lblMAR = new System.Windows.Forms.Label();
+            this.lblIQ6 = new System.Windows.Forms.Label();
+            this.lblIQ5 = new System.Windows.Forms.Label();
+            this.lblIQ4 = new System.Windows.Forms.Label();
+            this.lblIQ3 = new System.Windows.Forms.Label();
+            this.lblIQ2 = new System.Windows.Forms.Label();
+            this.lblIQ1 = new System.Windows.Forms.Label();
+            this.lblSP = new System.Windows.Forms.Label();
+            this.lblBP = new System.Windows.Forms.Label();
+            this.lblSI = new System.Windows.Forms.Label();
+            this.lblDI = new System.Windows.Forms.Label();
+            this.lblAH = new System.Windows.Forms.Label();
+            this.lblAL = new System.Windows.Forms.Label();
+            this.lblBL = new System.Windows.Forms.Label();
+            this.lblBH = new System.Windows.Forms.Label();
+            this.lblDL = new System.Windows.Forms.Label();
+            this.lblDH = new System.Windows.Forms.Label();
+            this.lblCL = new System.Windows.Forms.Label();
+            this.lblCH = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,7 +60,7 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1011, 586);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -97,23 +115,258 @@
             this.lblMAR.Text = "00000000";
             this.lblMAR.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // lblIQ6
+            // 
+            this.lblIQ6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblIQ6.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIQ6.ForeColor = System.Drawing.Color.White;
+            this.lblIQ6.Location = new System.Drawing.Point(428, 122);
+            this.lblIQ6.Name = "lblIQ6";
+            this.lblIQ6.Size = new System.Drawing.Size(86, 22);
+            this.lblIQ6.TabIndex = 5;
+            this.lblIQ6.Text = "00000000";
+            this.lblIQ6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblIQ5
+            // 
+            this.lblIQ5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblIQ5.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIQ5.ForeColor = System.Drawing.Color.White;
+            this.lblIQ5.Location = new System.Drawing.Point(428, 146);
+            this.lblIQ5.Name = "lblIQ5";
+            this.lblIQ5.Size = new System.Drawing.Size(86, 22);
+            this.lblIQ5.TabIndex = 6;
+            this.lblIQ5.Text = "00000000";
+            this.lblIQ5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblIQ4
+            // 
+            this.lblIQ4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblIQ4.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIQ4.ForeColor = System.Drawing.Color.White;
+            this.lblIQ4.Location = new System.Drawing.Point(428, 169);
+            this.lblIQ4.Name = "lblIQ4";
+            this.lblIQ4.Size = new System.Drawing.Size(86, 21);
+            this.lblIQ4.TabIndex = 7;
+            this.lblIQ4.Text = "00000000";
+            this.lblIQ4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblIQ3
+            // 
+            this.lblIQ3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblIQ3.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIQ3.ForeColor = System.Drawing.Color.White;
+            this.lblIQ3.Location = new System.Drawing.Point(428, 191);
+            this.lblIQ3.Name = "lblIQ3";
+            this.lblIQ3.Size = new System.Drawing.Size(86, 21);
+            this.lblIQ3.TabIndex = 8;
+            this.lblIQ3.Text = "00000000";
+            this.lblIQ3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblIQ2
+            // 
+            this.lblIQ2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblIQ2.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIQ2.ForeColor = System.Drawing.Color.White;
+            this.lblIQ2.Location = new System.Drawing.Point(428, 214);
+            this.lblIQ2.Name = "lblIQ2";
+            this.lblIQ2.Size = new System.Drawing.Size(86, 20);
+            this.lblIQ2.TabIndex = 9;
+            this.lblIQ2.Text = "00000000";
+            this.lblIQ2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblIQ1
+            // 
+            this.lblIQ1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblIQ1.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIQ1.ForeColor = System.Drawing.Color.White;
+            this.lblIQ1.Location = new System.Drawing.Point(428, 236);
+            this.lblIQ1.Name = "lblIQ1";
+            this.lblIQ1.Size = new System.Drawing.Size(86, 21);
+            this.lblIQ1.TabIndex = 10;
+            this.lblIQ1.Text = "00000000";
+            this.lblIQ1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSP
+            // 
+            this.lblSP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblSP.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSP.ForeColor = System.Drawing.Color.White;
+            this.lblSP.Location = new System.Drawing.Point(115, 127);
+            this.lblSP.Name = "lblSP";
+            this.lblSP.Size = new System.Drawing.Size(83, 20);
+            this.lblSP.TabIndex = 11;
+            this.lblSP.Text = "00000000";
+            this.lblSP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBP
+            // 
+            this.lblBP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblBP.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBP.ForeColor = System.Drawing.Color.White;
+            this.lblBP.Location = new System.Drawing.Point(115, 151);
+            this.lblBP.Name = "lblBP";
+            this.lblBP.Size = new System.Drawing.Size(83, 19);
+            this.lblBP.TabIndex = 12;
+            this.lblBP.Text = "00000000";
+            this.lblBP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSI
+            // 
+            this.lblSI.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblSI.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSI.ForeColor = System.Drawing.Color.White;
+            this.lblSI.Location = new System.Drawing.Point(115, 172);
+            this.lblSI.Name = "lblSI";
+            this.lblSI.Size = new System.Drawing.Size(83, 19);
+            this.lblSI.TabIndex = 13;
+            this.lblSI.Text = "00000000";
+            this.lblSI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDI
+            // 
+            this.lblDI.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblDI.Font = new System.Drawing.Font("Roboto Slab", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDI.ForeColor = System.Drawing.Color.White;
+            this.lblDI.Location = new System.Drawing.Point(115, 193);
+            this.lblDI.Name = "lblDI";
+            this.lblDI.Size = new System.Drawing.Size(83, 19);
+            this.lblDI.TabIndex = 14;
+            this.lblDI.Text = "00000000";
+            this.lblDI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAH
+            // 
+            this.lblAH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblAH.Font = new System.Drawing.Font("Roboto Slab", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAH.ForeColor = System.Drawing.Color.White;
+            this.lblAH.Location = new System.Drawing.Point(116, 36);
+            this.lblAH.Name = "lblAH";
+            this.lblAH.Size = new System.Drawing.Size(37, 20);
+            this.lblAH.TabIndex = 15;
+            this.lblAH.Text = "0000";
+            this.lblAH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAL
+            // 
+            this.lblAL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblAL.Font = new System.Drawing.Font("Roboto Slab", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAL.ForeColor = System.Drawing.Color.White;
+            this.lblAL.Location = new System.Drawing.Point(159, 36);
+            this.lblAL.Name = "lblAL";
+            this.lblAL.Size = new System.Drawing.Size(37, 20);
+            this.lblAL.TabIndex = 16;
+            this.lblAL.Text = "0000";
+            this.lblAL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBL
+            // 
+            this.lblBL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblBL.Font = new System.Drawing.Font("Roboto Slab", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBL.ForeColor = System.Drawing.Color.White;
+            this.lblBL.Location = new System.Drawing.Point(159, 60);
+            this.lblBL.Name = "lblBL";
+            this.lblBL.Size = new System.Drawing.Size(37, 20);
+            this.lblBL.TabIndex = 18;
+            this.lblBL.Text = "0000";
+            this.lblBL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblBH
+            // 
+            this.lblBH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblBH.Font = new System.Drawing.Font("Roboto Slab", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBH.ForeColor = System.Drawing.Color.White;
+            this.lblBH.Location = new System.Drawing.Point(116, 60);
+            this.lblBH.Name = "lblBH";
+            this.lblBH.Size = new System.Drawing.Size(37, 20);
+            this.lblBH.TabIndex = 17;
+            this.lblBH.Text = "0000";
+            this.lblBH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDL
+            // 
+            this.lblDL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblDL.Font = new System.Drawing.Font("Roboto Slab", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDL.ForeColor = System.Drawing.Color.White;
+            this.lblDL.Location = new System.Drawing.Point(159, 106);
+            this.lblDL.Name = "lblDL";
+            this.lblDL.Size = new System.Drawing.Size(37, 20);
+            this.lblDL.TabIndex = 22;
+            this.lblDL.Text = "0000";
+            this.lblDL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDH
+            // 
+            this.lblDH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblDH.Font = new System.Drawing.Font("Roboto Slab", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDH.ForeColor = System.Drawing.Color.White;
+            this.lblDH.Location = new System.Drawing.Point(116, 106);
+            this.lblDH.Name = "lblDH";
+            this.lblDH.Size = new System.Drawing.Size(37, 20);
+            this.lblDH.TabIndex = 21;
+            this.lblDH.Text = "0000";
+            this.lblDH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCL
+            // 
+            this.lblCL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblCL.Font = new System.Drawing.Font("Roboto Slab", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCL.ForeColor = System.Drawing.Color.White;
+            this.lblCL.Location = new System.Drawing.Point(159, 82);
+            this.lblCL.Name = "lblCL";
+            this.lblCL.Size = new System.Drawing.Size(37, 20);
+            this.lblCL.TabIndex = 20;
+            this.lblCL.Text = "0000";
+            this.lblCL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCH
+            // 
+            this.lblCH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
+            this.lblCH.Font = new System.Drawing.Font("Roboto Slab", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCH.ForeColor = System.Drawing.Color.White;
+            this.lblCH.Location = new System.Drawing.Point(116, 82);
+            this.lblCH.Name = "lblCH";
+            this.lblCH.Size = new System.Drawing.Size(37, 20);
+            this.lblCH.TabIndex = 19;
+            this.lblCH.Text = "0000";
+            this.lblCH.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1011, 586);
+            this.Controls.Add(this.lblDL);
+            this.Controls.Add(this.lblDH);
+            this.Controls.Add(this.lblCL);
+            this.Controls.Add(this.lblCH);
+            this.Controls.Add(this.lblBL);
+            this.Controls.Add(this.lblBH);
+            this.Controls.Add(this.lblAL);
+            this.Controls.Add(this.lblAH);
+            this.Controls.Add(this.lblDI);
+            this.Controls.Add(this.lblSI);
+            this.Controls.Add(this.lblBP);
+            this.Controls.Add(this.lblSP);
+            this.Controls.Add(this.lblIQ1);
+            this.Controls.Add(this.lblIQ2);
+            this.Controls.Add(this.lblIQ3);
+            this.Controls.Add(this.lblIQ4);
+            this.Controls.Add(this.lblIQ5);
+            this.Controls.Add(this.lblIQ6);
             this.Controls.Add(this.lblMAR);
             this.Controls.Add(this.lblMBR);
             this.Controls.Add(this.lblPC);
             this.Controls.Add(this.lblIR);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(1028, 631);
             this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(1027, 597);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -127,6 +380,24 @@
         private System.Windows.Forms.Label lblPC;
         private System.Windows.Forms.Label lblMBR;
         private System.Windows.Forms.Label lblMAR;
+        private System.Windows.Forms.Label lblIQ6;
+        private System.Windows.Forms.Label lblIQ5;
+        private System.Windows.Forms.Label lblIQ4;
+        private System.Windows.Forms.Label lblIQ3;
+        private System.Windows.Forms.Label lblIQ2;
+        private System.Windows.Forms.Label lblIQ1;
+        private System.Windows.Forms.Label lblSP;
+        private System.Windows.Forms.Label lblBP;
+        private System.Windows.Forms.Label lblSI;
+        private System.Windows.Forms.Label lblDI;
+        private System.Windows.Forms.Label lblAH;
+        private System.Windows.Forms.Label lblAL;
+        private System.Windows.Forms.Label lblBL;
+        private System.Windows.Forms.Label lblBH;
+        private System.Windows.Forms.Label lblDL;
+        private System.Windows.Forms.Label lblDH;
+        private System.Windows.Forms.Label lblCL;
+        private System.Windows.Forms.Label lblCH;
     }
 }
 
